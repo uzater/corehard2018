19142433771dcfb0a8904c72050cc04ffaa817d5
uzater <uzater@yandex.ru>
2018-11-02 14:16:49 +0300

    package 2

--- conanfile.py
+++ conanfile.py
@@ -1,42 +1,29 @@
 from conans import ConanFile, CMake, tools
 
 
-class Corehard2018Conan(ConanFile):
-    name = "corehard2018"
+class HelloConan(ConanFile):
+    name = "hello"
     version = "0.1.0"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Corehard2018 here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    license = "MIT"
+    url = "https://github.com/sdukshis/ci-corehard"
+    description = "Demo CI/CD workshop project"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
     generators = "cmake"
-
-    def source(self):
-        self.run("git clone https://github.com/memsharded/hello.git")
-        self.run("cd hello && git checkout static_shared")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-                              '''PROJECT(MyHello)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+    requires = "gtest/1.8.1@bincrafters/stable"
+    exports_sources = [
+        "include/*",
+        "src/*",
+        "tests/*",
+        "CMakeLists.txt",
+    ]
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="hello")
+        cmake.configure()
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
+        self.copy("*.h", dst="include", src="include")
         self.copy("*hello.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
